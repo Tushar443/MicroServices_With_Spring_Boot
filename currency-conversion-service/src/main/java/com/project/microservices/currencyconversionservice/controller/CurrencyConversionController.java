84fa2a66906edc304fd792f2cdf386891bae5f9b
Tushar More <65149782+Tushar443@users.noreply.github.com>
2024-03-05 20:42:28 +0530

    Added Feign and service registry ,Load balancing

--- currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.project.microservices.currencyconversionservice.controller;
 
 import com.project.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.project.microservices.currencyconversionservice.service.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,15 +14,28 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
     //http://localhost:8000/currency-conversion/from/USD/to/INR/quantity/10
+//    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+//    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,@PathVariable String to,
+//                                                          @PathVariable BigDecimal quantity){
+//        HashMap<String,String> uriVariables = new HashMap<>();
+//        uriVariables.put("from",from);
+//        uriVariables.put("to",to);
+//        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class,uriVariables);
+//        CurrencyConversion currencyConversion = responseEntity.getBody();
+//        return new CurrencyConversion(currencyConversion.getId()
+//                , from,to,quantity,
+//                currencyConversion.getConversionMultiple()
+//                ,quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
+//    }
+    // Feign Implementation
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,@PathVariable String to,
+    public CurrencyConversion calculateCurrencyConversionUsingFeign(@PathVariable String from,@PathVariable String to,
                                                           @PathVariable BigDecimal quantity){
-        HashMap<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("from",from);
-        uriVariables.put("to",to);
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class,uriVariables);
-        CurrencyConversion currencyConversion = responseEntity.getBody();
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retriveExchangeValue(from, to);
         return new CurrencyConversion(currencyConversion.getId()
                 , from,to,quantity,
                 currencyConversion.getConversionMultiple()
---
+++ currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/service/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.project.microservices.currencyconversionservice.service;
+
+import com.project.microservices.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "currency-exchange",url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retriveExchangeValue(@PathVariable String from, @PathVariable String to);
+
+    }
