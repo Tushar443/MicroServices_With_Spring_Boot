f0dc3c3cc1d1e1b78fd0d8261e4738fe33165859
Tushar More <65149782+Tushar443@users.noreply.github.com>
2024-03-09 12:46:53 +0530

    Spring cloud Gateway Added

---
+++ api-gateway/src/main/java/com/project/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,30 @@
+package com.project.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gateWayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p-> p.path("/get")
+                        .filters(f-> f
+                                .addRequestHeader("MyHeader","Added Header My URI")
+                                .addRequestParameter("param","myParamValue")
+                        )
+                        .uri("http://httpbin.org:80"))
+                .route(p->p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p->p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p->p.path("/currency-conversion-new/**")
+                        .filters(f->f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
